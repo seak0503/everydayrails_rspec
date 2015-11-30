797c6c3ec72629b875d18a7d2f52dd6b17044c67
shyamahira <shyamahira@bbtower.co.jp>
2015-11-30 11:13:12 +0900

    p39まで完了 続き: p39, 構文をシンプルにする

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,55 +1,44 @@
 require 'rails_helper'
 
 describe Contact do
-  it "is valid with a firstname, lastname and email" do
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname: 'Sumner',
-      email: 'tester@example.com')
-    expect(contact).to be_valid
+  example '有効なファクトリを持つこと' do
+    expect(FactoryGirl.build(:contact)).to be_valid
   end
 
-  it "is invalid without a firstname" do
-    contact = Contact.new(firstname: nil)
+  example '名がなければ無効な状態であること' do
+    contact = FactoryGirl.build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
-  it "is invalid without a lastname" do
-    contact = Contact.new(lastname: nil)
+  example '性がなければ無効な状態であること' do
+    contact = FactoryGirl.build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
-  it "is invalid without an email address" do
-    contact = Contact.new(email: nil)
+  example 'メールアドレスがなければ無効な状態であること' do
+    contact = FactoryGirl.build(:contact, email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
 
-  it "is invalid with a duplicate email address" do
-    Contact.create(
-      firstname: 'Joe', lastname: 'Tester',
-      email: 'tester@example.com'
-    )
-    contact = Contact.new(
-      firstname: 'Jane', lastname: 'Tester',
-      email: 'tester@example.com'
-    )
+  example '重複したメールアドレスなら無効な状態であること' do
+    FactoryGirl.create(:contact, email: 'aaron@example.com')
+    contact = FactoryGirl.build(:contact, email: 'aaron@example.com')
     contact.valid?
     expect(contact.errors[:email]).to include("has already been taken")
   end
 
-  it "returns a contact's full name as a string" do
-    contact = Contact.new(
-      firstname: 'John',
-      lastname: 'Doe',
-      email: 'johndoe@example.com'
+  example '連絡先のフルネームを文字列として返すこと' do
+    contact = FactoryGirl.build(:contact,
+      firstname: 'Jane',
+      lastname: 'Smith'
     )
-    expect(contact.name).to eq 'John Doe'
+    expect(contact.name).to eq 'Jane Smith'
   end
 
-  describe "filter last name by letter" do
+  describe '文字で性をフィルタする' do
     before :each do
       @smith = Contact.create(
         firstname: 'John',
@@ -68,14 +57,14 @@ describe Contact do
       )
     end
 
-    context "with matching letters" do
-      it "returns a sorted array of results that match" do
+    context 'マッチする文字の場合' do
+      example 'マッチした結果をソート済みの配列として返すこと' do
         expect(Contact.by_letter("J")).to eq [@johnson, @jones]
       end
     end
 
-    context "with non-matching letters" do
-      it "omits results that do not match" do
+    context 'マッチしない文字の場合' do
+      example 'マッチしなかったものは結果に含まれないこと' do
         expect(Contact.by_letter("J")).not_to include @smith
       end
     end
