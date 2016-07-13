3babccbfd72835915df40dacbc6704e40a2e317e
shyamahira <shyamahira@bbtower.co.jp>
2016-07-13 17:43:36 +0900

    P21まで完了

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -9,14 +9,42 @@ describe Contact do
       email: 'tester@example.com')
     expect(contact).to be_valid
   end
+
   # 名がなければ無効な状態であること
-  it "is invalid without a firstname"
+  it "is invalid without a firstname" do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
+
   # 姓がなければ無効な状態であること
-  it "is invalid without a lastname"
+  it "is invalid without a lastname" do
+    contact = Contact.new(lastname: nil)
+    contact.valid?
+    expect(contact.errors[:lastname]).to include("can't be blank")
+  end
+
   # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email address"
+  it "is invalid without an email address" do
+    contact = Contact.new(email: nil)
+    contact.valid?
+    expect(contact.errors[:email]).to include("can't be blank")
+  end
+
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address"
+  it "is invalid with a duplicate email address" do
+    Contact.create(
+      firstname: 'Joe', lastname: 'Tester',
+      email: "tester@example.com"
+    )
+    contact = Contact.new(
+      firstname: 'Jane', lastname: 'Tester',
+      email: "tester@example.com"
+    )
+    contact.valid?
+    expect(contact.errors[:email]).to include("has already been taken")
+  end
+
   # 連絡先のフルネームを文字列として返すこと
   it "returns a contact's full name as a string"
 end
