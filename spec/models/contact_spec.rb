40f9c46b6fa46252db74873ec29665f80fe68237
shyamahira <shyamahira@bbtower.co.jp>
2016-07-20 14:27:33 +0900

    P36まで完了

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,32 +1,28 @@
 require 'rails_helper'
 
 describe Contact do
-  # 姓と名とメールがあれば有効な状態であること
-  it "is valid with a firstname, lastname and email" do
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname: 'Sumner',
-      email: 'tester@example.com')
-    expect(contact).to be_valid
+  # 有効なファクトリを持つこと
+  it 'has a valid factory' do
+    expect(FactoryGirl.build(:contact)).to be_valid
   end
 
   # 名がなければ無効な状態であること
   it "is invalid without a firstname" do
-    contact = Contact.new(firstname: nil)
+    contact = FactoryGirl.build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
   # 姓がなければ無効な状態であること
   it "is invalid without a lastname" do
-    contact = Contact.new(lastname: nil)
+    contact = FactoryGirl.build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
   # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address" do
-    contact = Contact.new(email: nil)
+    contact = FactoryGirl.build(:contact, email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
@@ -47,12 +43,11 @@ describe Contact do
 
   # 連絡先のフルネームを文字列として返すこと
   it "returns a contact's full name as a string" do
-    contact = Contact.new(
-      firstname: 'John',
-      lastname: 'Doe',
-      email: 'johndoe@example.com'
+    contact = FactoryGirl.build(:contact,
+      firstname: "Jane",
+      lastname: "Smith"
     )
-    expect(contact.name).to eq 'John Doe'
+    expect(contact.name).to eq 'Jane Smith'
   end
 
   # 文字で姓をフィルタする
