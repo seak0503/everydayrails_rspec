4e9ac85852c0cdc09e9d7760c9f486522f8b53fa
shyamahira <shyamahira@bbtower.co.jp>
2016-07-21 14:39:57 +0900

    P41まで完了

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -3,17 +3,13 @@ require "rails_helper"
 describe Phone do
   # 連絡先ごとに重複した電話番号を許可しないこと
   it "does not allow duplicate phone numbers per contact" do
-    contact = Contact.create(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'joetester@example.com'
-    )
-    contact.phones.create(
-      phone_type: 'home',
+    contact = create(:contact)
+    create(:home_phone,
+      contact: contact,
       phone: '785-555-1234'
     )
-    mobile_phone = contact.phones.build(
-      phone_type: 'mobile',
+    mobile_phone = build(:mobile_phone,
+      contact: contact,
       phone: '785-555-1234'
     )
 
@@ -23,21 +19,9 @@ describe Phone do
 
   # 2件の連絡先で同じ電話番号を共有できること
   it 'allows two contacts to share a phone number' do
-    contact = Contact.create(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'joetester@example.com'
-    )
-    contact.phones.create(
-      phone_type: 'home',
+    create(:home_phone,
       phone: '785-555-1234'
     )
-    other_contact = Contact.new
-    other_phone = other_contact.phones.build(
-      phone_type: 'home',
-      phone: '785-555-1234'
-    )
-
-    expect(other_phone).to be_valid
+    expect(build(:home_phone, phone: '785-555-1234')).to be_valid
   end
 end
