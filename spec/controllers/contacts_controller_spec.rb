4a02eea40f14ec0e9f8b8a3c1702933db591c8b9
shyamahira <shyamahira@bbtower.co.jp>
2016-07-30 00:40:25 +0900

    p64 user accessのshowメソッドテストまで完了

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -194,4 +194,55 @@ describe ContactsController do
       end
     end
   end
+
+  describe 'user access' do
+    before do
+      user = create(:user)
+      session[:user_id] = user.id
+    end
+
+    describe 'GET #index' do
+      context 'with params[:letter]' do
+        it 'populates an array of contacts starting with the letter' do
+          smith = create(:contact, lastname: 'Smith')
+          jones = create(:contact, lastname: 'Jones')
+          get :index, letter: 'S'
+          expect(assigns(:contacts)).to match_array([smith])
+        end
+
+        it 'renders th :index template' do
+          get :index, letter: 'S'
+          expect(response).to render_template :index
+        end
+      end
+
+      context 'without params[:letter]' do
+        it 'populates an array of all contacts' do
+          smith = create(:contact, lastname: 'Smith')
+          jones = create(:contact, lastname: 'Jones')
+          get :index
+          expect(assigns(:contacts)).to match_array([smith, jones])
+        end
+
+        it 'renders the :index template' do
+          get :index
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    describe 'GET #show' do
+      it 'assigns the requested contact to @contact' do
+        contact = create(:contact)
+        get :show, id: contact
+        expect(assigns(:contact)).to eq contact
+      end
+
+      it 'renders the :show template' do
+        contact = create(:contact)
+        get :show, id: contact
+        expect(response).to render_template :show
+      end
+    end
+  end
 end
