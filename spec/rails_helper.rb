20af3412a87d00771f5e3337deff2947be6f98a9
shyamahira <shyamahira@bbtower.co.jp>
2016-08-09 09:03:37 +0900

    8. フィーチャスペック完了

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,22 @@ RSpec.configure do |config|
   # Include custom login macros
   config.include LoginMacros
 
+  # Configure DatabaseCleaner to reset data between tests
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
